6a5dc082ccc0fcc0ea1d9dfb811d25ad248a52ec
Natalia <zakrevskaya.97@inbox.ru>
2022-06-23 20:27:08 +0300

    fix FooterLogo

---
+++ src/components/Logo/footerLogo/FooterLogo.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../../../Images/footerLogo.svg';
+import style from './FooterLogo.module.scss';
+
+
+const FooterLogo = () => {
+  return (
+    <div className={style.logoBlock}>
+      <img src={ logo } alt={ 'logo' }/>
+      <p>Территория <span>ZOO</span></p>
+    </div>
+  );
+};
+
+export default FooterLogo;
