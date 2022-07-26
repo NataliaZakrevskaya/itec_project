42b934586f64163bca617e8b204eb0ca91987eef
Natalia <zakrevskaya.97@inbox.ru>
2022-07-26 12:29:31 +0300

    fix total basket count

--- src/redux/reducers/basket-reducer.ts
+++ src/redux/reducers/basket-reducer.ts
@@ -12,7 +12,12 @@ export const slice = createSlice( {
     setProductToBasket( state, action: PayloadAction<{ product: ProductItemType }> ) {
       state.productsInBasket.unshift( action.payload.product );
       state.totalProductsCount = state.productsInBasket.length;
-    },
+        state.totalSum = state.productsInBasket.map( (product: ProductItemType) => {
+          if(product.chosen_option){
+          return +product.chosen_option.price }
+          return +product.options[0].price
+        }).reduce( ( acc, current ) => acc + current, 0 );
+        },
     removeProductFromBasket( state, action: PayloadAction<{ id: number }> ) {
       state.productsInBasket = state.productsInBasket.filter( product => product.id !== action.payload.id );
       state.totalProductsCount = state.productsInBasket.length;
