6be106c85f907536c867baaaa3d29f004da2ffd5
Natalia <zakrevskaya.97@inbox.ru>
2022-08-05 20:39:26 +0300

    fix option price in product page

--- src/components/common/UnitsForBasket/UnitsForBasket.tsx
+++ src/components/common/UnitsForBasket/UnitsForBasket.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import style from '../../../pages/ProductPage/ProductPage.module.scss';
 import { OptionType } from '../../../mocks';
+import { getPrice } from '../../../helpers/getPrice';
 
 const UnitsForBasket = ( { option, active, onUnitClick }: UnitsForBasketPropsType ) => {
-  const onClick = () => {
-    onUnitClick( option );
-  };
-
+  const price = getPrice( +option.price );
   return (
     <>
       { !option.partial &&
-        <div onClick={ onClick } className={ active ? style.active : style.unit }>
+        <div onClick={ () => onUnitClick( option ) } className={ active ? style.active : style.unit }>
           <p className={ style.option }>{ option.size } { option.units.unit_name }</p>
-          <p className={ style.price }>{ option.price } BYN</p>
+          <p className={ style.price }>{ price } BYN</p>
         </div>
       }
     </>
