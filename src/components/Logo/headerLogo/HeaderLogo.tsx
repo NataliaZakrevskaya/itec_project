c8a92c5f23fa8db9080e2539fa987920d71723e8
Natalia <zakrevskaya.97@inbox.ru>
2022-09-08 13:11:59 +0300

    fix onclick for header logo

--- src/components/Logo/headerLogo/HeaderLogo.tsx
+++ src/components/Logo/headerLogo/HeaderLogo.tsx
@@ -1,13 +1,20 @@
 import React, { ReactElement } from 'react';
 import style from './HeaderLogo.module.scss';
+import { useNavigate } from 'react-router-dom';
+import { routesPathsEnum } from '../../../routes/enums';
 
-const HeaderLogo = React.memo((): ReactElement => {
+const HeaderLogo = React.memo( (): ReactElement => {
+  const navigate = useNavigate();
+  const onLogoClick = () => {
+    navigate( routesPathsEnum.MAIN );
+  };
+
   return (
-    <div className={ style.logoBlock }>
+    <div className={ style.logoBlock } onClick={ onLogoClick }>
       <div/>
       <p>Территория <span>ZOO</span></p>
     </div>
   );
-});
+} );
 
 export default HeaderLogo;
