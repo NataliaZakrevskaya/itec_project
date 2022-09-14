084961de70defa747fdeff3230834425ec2e38e6
Natalia <zakrevskaya.97@inbox.ru>
2022-09-14 15:45:43 +0300

    fix validation for name input for one click order modal

--- src/components/common/modals/OneClickOrder/OneClickOrder.tsx
+++ src/components/common/modals/OneClickOrder/OneClickOrder.tsx
@@ -48,7 +48,11 @@ const OneClickOrder = ( { closeOneClickOrderModal }: OnClickOrderPropsType ): Re
     validate: ( values ) => {
       const errors: FormikErrorType = {};
       if ( values.name.length < 2 ) {
-        errors.name = 'Поле обязательно для заполнения';
+        errors.name = 'Минимально допустимое количество символов: 2';
+      } else if (!/(^(?!~!"№;%\?.*\(\)#\$%\^&=\+-_@$)([A-Za-z]{1}[a-z]{1,18}( [A-Za-z]{1})?([a-z]{1,18})?)$)|(^[А-Яа-я]{1}[а-я]{1,18}( [А-Яа-я]{1})?([а-я]{1,18})?$)/i.test(values.name)){
+        errors.name = 'Допустимые символы: A-z А-я';
+      } else if (values.name.length > 30) {
+        errors.name = 'Максимально допустимое количество символов: 30';
       }
       if ( !values.phoneNumber ) {
         errors.phoneNumber = 'Поле обязательно для заполнения';
@@ -95,7 +99,7 @@ const OneClickOrder = ( { closeOneClickOrderModal }: OnClickOrderPropsType ): Re
                 <p>Имя</p>
                 <input
                   type={ 'name' }
-                  placeholder={ 'Иванов Иван Иванович' }
+                  placeholder={ 'Иванов Иван' }
                   { ...formik.getFieldProps( 'name' ) }
                 />
                 { formik.touched.name && formik.errors.name &&
