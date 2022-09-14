95e713bd3f7553fd78ea790daf71d622b5725c4d
Natalia <zakrevskaya.97@inbox.ru>
2022-09-14 14:22:16 +0300

    fix cursor for FooterLogo.tsx

--- src/components/Logo/footerLogo/FooterLogo.tsx
+++ src/components/Logo/footerLogo/FooterLogo.tsx
@@ -1,10 +1,16 @@
 import React, { ReactElement } from 'react';
 import logo from '../../../Images/footerLogo.svg';
 import style from './FooterLogo.module.scss';
+import { useNavigate } from 'react-router-dom';
+import { routesPathsEnum } from '../../../routes/enums';
 
 const FooterLogo = React.memo((): ReactElement => {
+  const navigate = useNavigate();
+  const onLogoClick = () => {
+    navigate( routesPathsEnum.MAIN );
+  };
   return (
-    <div className={ style.logoBlock }>
+    <div className={ style.logoBlock } onClick={onLogoClick}>
       <img src={ logo } loading={'lazy'} alt={ 'logo' }/>
       <p>Территория <span>ZOO</span></p>
     </div>
