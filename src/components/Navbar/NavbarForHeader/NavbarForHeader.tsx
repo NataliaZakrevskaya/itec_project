535c9029a8f9541f73e500fd59dffa6da735803a
Natalia <zakrevskaya.97@inbox.ru>
2022-10-13 16:29:25 +0300

    add possibility of navigate to catalog page when you click on discounts link

--- src/components/Navbar/NavbarForHeader/NavbarForHeader.tsx
+++ src/components/Navbar/NavbarForHeader/NavbarForHeader.tsx
@@ -1,16 +1,19 @@
 import React, { ReactElement } from 'react';
 import { routesPathsEnum } from '../../../routes/enums';
 import { NavLink, useNavigate } from 'react-router-dom';
-import { HashLink } from 'react-router-hash-link';
 import style from './NavbarForHeader.module.scss';
 import { NavbarForHeaderPropsType } from '../types';
+import { useDispatch } from 'react-redux';
+import { setChosenDiscountFilterStatus } from '../../../redux/reducers/discountFilter';
 
 const NavbarForHeader = React.memo( ( { forHeaderBurger, closeEditMode }: NavbarForHeaderPropsType ): ReactElement => {
 
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const discountLinkHandler = () => {
     forHeaderBurger && closeEditMode();
-    navigate( routesPathsEnum.MAIN );
+    dispatch( setChosenDiscountFilterStatus( { filterStatus: true } ) );
+    navigate( routesPathsEnum.CATALOG );
   };
 
   return (
@@ -21,9 +24,8 @@ const NavbarForHeader = React.memo( ( { forHeaderBurger, closeEditMode }: Navbar
       <NavLink className={ ( navData ) => navData.isActive ? style.activeLink : '' }
                onClick={ () => closeEditMode() }
                to={ routesPathsEnum.CATALOG }>Каталог</NavLink>
-      <div onClick={ discountLinkHandler } className={ style.scroll }>
-        <HashLink to={ '#discounts' } className={ style.anchor }
-                  scroll={ ( el ) => el.scrollIntoView( { behavior: 'smooth', block: 'center' } ) }>Акции</HashLink>
+      <div onClick={ discountLinkHandler } className={ style.anchor }>
+        Акции
       </div>
       <NavLink className={ ( navData ) => navData.isActive ? style.activeLink : '' }
                onClick={ () => closeEditMode() }
