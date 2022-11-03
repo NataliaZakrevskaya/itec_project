f25fdc89223fda65167f83a6d6572e9c19416c09
Natalia <zakrevskaya.97@inbox.ru>
2022-11-03 17:25:03 +0300

    change token url and refresh url

--- src/Api/auth/constants/index.ts
+++ src/Api/auth/constants/index.ts
@@ -1,2 +1,2 @@
-export const TOKEN_URL = 'api/token/';
-export const REFRESH_TOKEN_URL = 'api/token/refresh/';
+export const TOKEN_URL = 'token/';
+export const REFRESH_TOKEN_URL = 'token/refresh/';
