d67969bfe8437d617ebf9b242268eff9ad3d194c
Natalia <zakrevskaya.97@inbox.ru>
2022-11-09 13:51:46 +0300

    fix alt for brands images

--- src/components/PopularBrandsBlock/Brand/Brand.tsx
+++ src/components/PopularBrandsBlock/Brand/Brand.tsx
@@ -2,12 +2,12 @@ import React, { ReactElement } from 'react';
 import style from './Brand.module.scss';
 import { BrandPropsType } from './types';
 
-const Brand = React.memo(( { id, image, chooseBrand, forBlock }: BrandPropsType ): ReactElement => {
+const Brand = React.memo( ( { id, name, image, chooseBrand, forBlock }: BrandPropsType ): ReactElement => {
   return (
-    <div className={forBlock ? style.brand : style.brandForPage } onClick={ () => chooseBrand( id ) }>
-      <img src={ image } loading={'lazy'} alt={ 'brand' } draggable="false"/>
+    <div className={ forBlock ? style.brand : style.brandForPage } onClick={ () => chooseBrand( id ) }>
+      <img src={ image } loading={ 'lazy' } alt={ name } draggable="false"/>
     </div>
   );
-});
+} );
 
 export default Brand;
